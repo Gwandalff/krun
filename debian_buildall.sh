51856b0de151c3c3c0063fad0dfb8f908b21bfca
Manuel Leduc <manuel.leduc@irisa.fr>
2019-07-04 16:17:46 +0200

    trying to reproduce

--- debian_buildall.sh
+++ debian_buildall.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
 export NO_MSRS=1
-#export ENABLE_JAVA=1
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export BENCH_OPTS=--no-tickless-check
 
 git clean -fxd
 sudo apt install -y virt-what python-cffi build-essential cpufrequtils cpuset linux-headers-$(uname -r) util-linux mst-tools policykit-1
-make clean
+make
 make JAVA_CPPFLAGS='"-I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux"' JAVA_LDFLAGS=-L${JAVA_HOME}/lib ENABLE_JAVA=1
 cd examples/benchmarks
-make clean
 make
 make java-bench
 cd ..
