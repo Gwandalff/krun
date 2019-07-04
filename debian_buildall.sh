0e6caa200d8aa29f49277617c2c78c8eaada6243
Manuel Leduc <manuel.leduc@irisa.fr>
2019-07-04 16:49:24 +0200

    another more minimal try

--- debian_buildall.sh
+++ debian_buildall.sh
@@ -6,23 +6,11 @@ export BENCH_OPTS=--no-tickless-check
 
 git clean -fxd
 sudo apt install -y virt-what python-cffi build-essential cpufrequtils cpuset linux-headers-$(uname -r) util-linux msr-tools policykit-1
-make
+#make
+#make clean
+ENABLE_JAVA=1 make 
 cd examples/benchmarks
 make
-#make java-bench
-cd ..
-
-cd ~/krun
-
-echo "I'ME HERE"
-pwd
-echo "I CLEAN"
-make clean
-echo "I BUILD"
-make JAVA_CPPFLAGS='"-I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux"' JAVA_LDFLAGS=-L${JAVA_HOME}/lib ENABLE_JAVA=1
-
-cd examples/benchmarks
 make java-bench
 cd ..
 ../krun.py java.krun $BENCH_OPTS
-
