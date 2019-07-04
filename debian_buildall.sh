46929faae979e487ade81a755155925d11156f1e
Manuel Leduc <manuel.leduc@irisa.fr>
2019-07-04 15:17:11 +0200

    add filesystem raz

--- debian_buildall.sh
+++ debian_buildall.sh
@@ -5,6 +5,7 @@ export ENABLE_JAVA=1
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export BENCH_OPTS=--no-tickless-check
 
+git clean -fxd
 sudo apt install -y virt-what python-cffi build-essential cpufrequtils cpuset linux-headers-$(uname -r) util-linux mst-tools policykit-1
 make clean
 make JAVA_CPPFLAGS='"-I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux"' JAVA_LDFLAGS=-L${JAVA_HOME}/lib ENABLE_JAVA=1
@@ -13,5 +14,7 @@ make clean
 make
 make java-bench
 cd ..
+#rm -rf example_result.json example_result.json.bs2 example.log example.manifest 
+java_result.json java_result.json.bs2 java.log java.manifest
 ../krun.py java.krun $BENCH_OPTS
 
