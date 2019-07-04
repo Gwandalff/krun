caaeee7bfc531f4c9a8597d7dc0965bd95556635
Manuel Leduc <manuel.leduc@irisa.fr>
2019-07-04 16:19:25 +0200

    fixes

--- debian_buildall.sh
+++ debian_buildall.sh
@@ -5,14 +5,12 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export BENCH_OPTS=--no-tickless-check
 
 git clean -fxd
-sudo apt install -y virt-what python-cffi build-essential cpufrequtils cpuset linux-headers-$(uname -r) util-linux mst-tools policykit-1
+sudo apt install -y virt-what python-cffi build-essential cpufrequtils cpuset linux-headers-$(uname -r) util-linux msr-tools policykit-1
 make
 make JAVA_CPPFLAGS='"-I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux"' JAVA_LDFLAGS=-L${JAVA_HOME}/lib ENABLE_JAVA=1
 cd examples/benchmarks
 make
 make java-bench
 cd ..
-#rm -rf example_result.json example_result.json.bs2 example.log example.manifest 
-java_result.json java_result.json.bs2 java.log java.manifest
 ../krun.py java.krun $BENCH_OPTS
 
