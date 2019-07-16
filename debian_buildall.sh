08144b50c94282ed4ab25143031e7101e9e2be73
Manuel Leduc <manuel.leduc@gmail.com>
2019-07-16 13:48:40 +0200

    Update debian_buildall.sh

--- debian_buildall.sh
+++ debian_buildall.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export NO_MSRS=1
+#export NO_MSRS=1
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export BENCH_OPTS=--no-tickless-check 
 
 git clean -fxd
-sudo apt install -y virt-what python-cffi build-essential cpufrequtils cpuset linux-headers-$(uname -r) util-linux msr-tools policykit-1 openjdk-8-jdk
+sudo apt install -y virt-what python-cffi build-essential cpufrequtils cpuset linux-headers-4.9.0-9-all util-linux msr-tools policykit-1 openjdk-8-jdk
 make JAVA_CPPFLAGS='"-I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux"' JAVA_LDFLAGS=-L${JAVA_HOME}/lib ENABLE_JAVA=1
 cd examples/benchmarks
 make
