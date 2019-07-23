a42c44ab9e334bade1e942b87d5101004591e579
Gwendal Jouneaux <gwendal.jouneaux@.irisa.fr>
2019-07-23 11:59:48 +0200

    the create_krunEntry script generate all the java files for the benchs -> error print good javac cmd but doesn't exec them

--- examples/create_VMs.py
+++ examples/create_VMs.py
@@ -1,9 +1,3 @@
-from distutils.spawn import find_executable
-
-JAVA_BIN = find_executable("java")
-
-print(JAVA_BIN)
-
 JVMS_FOLDER = "/home/benchmarks/jvms/"
 BENCH_FOLDER = "/home/benchmarks/benchmark/"
 
@@ -30,8 +24,8 @@ for jvm in jvms:
 				#'vm_def': JavaJarVMDef(JVMS_FOLDER+jvm+".jar", BENCH_FOLDER+lang+"/"+pattern+"/benchmarks.jar"),
 				'JAVA_BIN': JVMS_FOLDER+jvm+"/bin/java",
 				'Bench' : BENCH_FOLDER+lang+"/"+pattern+"/benchmarks.jar",
-        			'variants': ['default-java'],
-        			'n_iterations': 1#ITERATIONS_ALL_VMS,
+        		'variants': ['default-java'],
+        		'n_iterations': 1 #ITERATIONS_ALL_VMS,
 			}
 			VMS[name] = krun_vm
 			for bench in BENCHMARKS:
--- examples/create_krunEntry.py
+++ examples/create_krunEntry.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+
 MODEL = '''import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
@@ -17,7 +20,7 @@ public class KrunEntry
         .measurementIterations(param)
         .resultFormat(ResultFormatType.JSON)
         .result("/home/benchmarks/results/{name}_" + System.currentTimeMillis() + ".json")
-        .param("program", new String[] {{ "/home/benchmarks/jvms/{name}.xmi" }})
+        .param("program", new String[] {{ "/home/benchmarks/programs/{name}.xmi" }})
         .build();
     try
     {{
@@ -30,7 +33,41 @@ public class KrunEntry
   }}
 }}'''
 
-tmp = MODEL.format(fullQualifiedInterpreter="test_FQN", name="test_name")
+def extractTestName(name):
+	parts = name.split(".")
+	return parts[0]
 
-print(tmp)
-"fr.mleduc.boa.BoaInterpreterBenchmark.boaInterpreter"
+patterns = ["interpreter", "revisitor", "switch", "visitor"]
+
+BENCH_FOR_JMH = "/home/benchmarks/benchmark/boa/interpreter/benchmarks.jar"
+
+ITERATION_PER_BENCH = 2
+
+BENCHMARKS = {}
+
+testNames = map(extractTestName, os.listdir("/home/benchmarks/programs/"))
+
+for testName in testNames:
+	testInfos = testName.split("_")
+	lang = testInfos[0]
+	test = testInfos[1]
+
+	for pattern in patterns:
+		krunName = lang+"_"+pattern+"_"+test
+		BENCHMARKS[krunName] = ITERATION_PER_BENCH
+
+		KRUN_ENTRY_DIR = "./benchmarks/"+krunName+"/java/"
+	
+		if not os.path.exists(KRUN_ENTRY_DIR):
+			os.makedirs(KRUN_ENTRY_DIR)
+			
+		f= open(KRUN_ENTRY_DIR+"KrunEntry.java","w+")
+		
+		fqn = "fr.mleduc." + lang + "." + lang.capitalize() + pattern.capitalize()+ "Benchmark." + lang + "Interpreter"
+
+		tmp = MODEL.format(fullQualifiedInterpreter=fqn, name=krunName)
+		f.write(tmp)
+		f.close
+		command = "javac -cp "+BENCH_FOR_JMH+":../iterations_runners/ " + KRUN_ENTRY_DIR+"KrunEntry.java"
+		print(command)
+		subprocess.run(command, shell=True)
